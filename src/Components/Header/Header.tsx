92b6eb32d4c4b4f148684ad140aff91274add9d2
Ravy Carvalho <164415227+ravycarvalhodeveloper@users.noreply.github.com>
2024-07-04 15:16:45 -0300

    Update Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
                </Fade>
 
                <Fade  direction="right" cascade damping={1.0}>
-                  <nav className='flex gap-10 mobile:hidden lg:flex'>
+                  <nav className='flex  mobile:hidden lg:flex'>
                             <Link to="/" className='text-doak-white font-custom font-bold hover:text-doak-grey hover:scale-125 transition-all'>Início</Link>
                             <Link to="/about" className='text-doak-white font-custom font-bold hover:text-doak-grey hover:scale-125 transition-all'>Sobre</Link>
                             <Link to="/contact" className='text-doak-white font-custom font-bold hover:text-doak-grey hover:scale-125 transition-all'>Contatos</Link>
